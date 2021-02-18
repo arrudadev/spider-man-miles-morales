13b2f6522b227c1d3641ec1d4ad531b3855e8d37
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-02-17 21:52:43 -0300

    style: add Lines animation

--- src/components/BackgroundLines/styles.ts
+++ src/components/BackgroundLines/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 import { ReactComponent as LinesSvg } from '../../assets/svg-lines.svg';
 
@@ -17,9 +17,29 @@ export const Section = styled.section`
   align-items: center;
 `;
 
+const Line = keyframes`
+  0%{
+    stroke-dasharray: 200 50;
+    stroke-dashoffset: 0;
+  }
+  100%{
+    stroke-dasharray: 500 5;
+    stroke-dashoffset: 1000;
+  }
+`;
+
 export const Lines = styled(LinesSvg)`
   width: 100%;
   border: 0px solid green;
+
+  & > g {
+    path.line1,
+    path.line2,
+    path.line3,
+    path.line4 {
+      animation: ${Line} 7s ease infinite alternate;
+    }
+  }
 
   @media screen and (max-width: 960px) {
     height: 200%;
